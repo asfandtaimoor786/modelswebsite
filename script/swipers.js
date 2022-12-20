d0b223c5c80cecfc402686f343544aecaaff3023
Taimoor <asfandtaimoor786@gmail.com>
2022-12-20 12:35:32 +0500

    upadte

--- script/swipers.js
+++ script/swipers.js
@@ -1,4 +1,29 @@
 /************************
+EXPLORE SWIPER
+************************/
+var swiper = new Swiper('.ExploreModelsSwiper', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+  loop: true,
+  centeredSlides: true,
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+/************************
 FOLLOW SWIPER
 ************************/
 var swiper = new Swiper('.FollowSwiper', {
@@ -37,7 +62,42 @@ var swiper = new Swiper('.FollowSwiper', {
 GALLERY SWIPER
 ************************/
 
-var swiper = new Swiper('.mySwiper-left', {
+var swiper = new Swiper('.GalleryRightSwiper', {
+  slidesPerView: 1.2,
+  spaceBetween: 10,
+  loop: true,
+  speed: 4000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2.5,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 8,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 9.8,
+      spaceBetween: 20,
+    },
+  },
+});
+var swiper = new Swiper('.GalleryLeftSwiper', {
   slidesPerView: 1.8,
   spaceBetween: 10,
   loop: true,
@@ -69,41 +129,6 @@ var swiper = new Swiper('.mySwiper-left', {
     },
     1200: {
       slidesPerView: 9.1,
-      spaceBetween: 20,
-    },
-  },
-});
-var swiper = new Swiper('.mySwiper-right', {
-  slidesPerView: 1.2,
-  spaceBetween: 10,
-  loop: true,
-  speed: 4000,
-  autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2.5,
-      spaceBetween: 10,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 20,
-    },
-    992: {
-      slidesPerView: 8,
-      spaceBetween: 20,
-    },
-    1200: {
-      slidesPerView: 9.8,
       spaceBetween: 20,
     },
   },
